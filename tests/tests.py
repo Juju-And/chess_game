9b94b024483c2e2687515a666ff46887b8b94490
Juju-And <justyna.andrejczuk@gmail.com>
2024-10-04 09:08:07 +0200

    add queen tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,5 @@
 from src.app import app
-from src.chess import King
+from src.chess import King, Queen
 import pytest
 
 
@@ -14,6 +14,19 @@ def test_king_valid_moves():
     king = King("f5")
     assert king.validate_move("e5") == "valid"
     assert king.validate_move("e8") == "not valid"
+
+
+def test_queen_available_moves():
+    queen = Queen("d4")
+    assert sorted(queen.list_available_moves) == sorted(
+        ["c4", "e4", "d5", "d3", "c5", "c3", "e5", "e3"]
+    )
+
+
+def test_queen_valid_moves():
+    queen = Queen("d4")
+    assert queen.validate_move("e4") == "valid"
+    assert queen.validate_move("e8") == "not valid"
 
 
 @pytest.fixture
